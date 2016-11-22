4f52cc4d82801ffc75ad17eadefef605bb8c640a
stormhouse <stormhouse86@gmail.com>
2016-11-22 19:57:58 +0800

    Lexer

---
+++ Lexer.py
@@ -0,0 +1,6 @@
+class Lexer:
+  def __init__(self):
+    print('init')
+
+if __name__ == '__main__':
+  print(Lexer)
--- Token.py
+++ Token.py
@@ -1,3 +1,7 @@
 class Token:
-    def __init__(self):
-        print('init')
+    EOL = '\\n'
+    def __init__(self, line):
+        self.lineNumber = line
+
+token = Token(123)
+print(token.lineNumber)
