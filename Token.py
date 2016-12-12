c8efa6b506876d33026e425cf2923c2683e7b8c7
stormhouse <stormhouse86@gmail.com>
2016-12-12 08:45:01 +0800

    py

--- Token.py
+++ Token.py
@@ -1,7 +1,15 @@
 class Token:
     EOL = '\\n'
     def __init__(self, line):
+        self.EOL = 'qq'
         self.lineNumber = line
+    def isIdentifier(self):
+        return False
 
 token = Token(123)
 print(token.lineNumber)
+print(token.EOL)
+print(Token.EOL)
+print(Token.isIdentifier)
+print(token.isIdentifier)
+print(token.isIdentifier())
---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+
+v = sys.version
+print(sys.version.index('\n'))
+print(v)
