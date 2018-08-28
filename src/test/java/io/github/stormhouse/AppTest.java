d73680a51ba24846f5a6aa15fe437bf1f78be245
stormhouse <stormhouse86@gmail.com>
2018-08-28 12:04:54 +0800

    by maven

---
+++ src/test/java/io/github/stormhouse/AppTest.java
@@ -0,0 +1,24 @@
+package io.github.stormhouse;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+    @Test
+    public void shouldAnswerWithTrue1() {
+        assertTrue(false);
+    }
+}
